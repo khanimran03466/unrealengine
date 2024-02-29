6abae9b2e0d9212e7f1a30b889877554b6ed4be0
imran khan <imran.ayub.khan@accenture.com>
2024-02-29 15:07:03 +0530

    animation added

--- js/custom.js
+++ js/custom.js
@@ -18,4 +18,18 @@ menuLi.forEach(el => {
         hamburger.classList.toggle("active");
         body.classList.toggle("active");
     })
-})
+});
+
+
+
+// Amination goes here.
+
+const timeline = gsap.timeline({ default: { duration: 1, ease: "bounce.out" } });
+
+timeline
+    .from(".video-text h1", { y: "-500%", delay: .5, opacity: 0 })
+    .from(".video-text a", { y: "-500%", delay: .5, opacity: 0 })
+    .from(".vertical-line", { x: "-500%", delay: .5, opacity: 0 })
+    .from(".about-us h2", { y: "500%", delay: .5, opacity: 0 })
+    .from(".about-us p", { y: "-500%", delay: .5, opacity: 0 })
+    .from(".community-left", { y: "-200%", delay: 2, opacity: 0 })
